536e2112d3abb344f87b3ce9202e491d0e8ef76e
Ken Finnigan <ken@kenfinnigan.me>
2012-05-22 13:06:52 -0400

    Switch to having a single portlet on a page, and the menu options are part of the layout instead of page

---
+++ war/src/main/java/org/gatein/admin/beans/MenuController.java
@@ -0,0 +1,22 @@
+package org.gatein.admin.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.exoplatform.portal.mop.SiteKey;
+import org.exoplatform.portal.mop.SiteType;
+import org.exoplatform.web.application.RequestContext;
+import org.exoplatform.web.url.navigation.NavigationResource;
+import org.exoplatform.web.url.navigation.NodeURL;
+
+@ManagedBean(name = "menu")
+@SessionScoped
+public class MenuController {
+
+	public String getNodeURL(String name) {
+		RequestContext context = RequestContext.getCurrentInstance();
+		NodeURL url = context.createURL(NodeURL.TYPE);
+		url.setResource(new NavigationResource(new SiteKey(SiteType.PORTAL, "admin"), name));
+		return url.toString();
+	}
+}
